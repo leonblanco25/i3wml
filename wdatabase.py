33cbe429ab94e9ce35c632ba1675f93cf65adce9
leo c <leonardo.cando25@hmail.com>
2020-05-29 23:32:43 -0400

    added .py extensions

--- wdatabase.py
+++ wdatabase.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 
 
-def wind_list():
+def wind_list(list):
     command = "wmctrl -l | awk '{if ($2==\"0\") print $1}' |tr \\n \" \""
     line = command  
 
@@ -16,12 +16,15 @@ def wind_list():
         output = subprocess.check_output(line, shell=True)
         #print("wind " +str(i) + " :" + str(output))
         line = command
-        list_cleanup(output)
+        list_cleanup(output, list)
         
     
-def list_cleanup(input):
+def list_cleanup(input,list):
         stripped_line = input. strip()
         line_list = stripped_line. split()
+        line_list = str(line_list).replace('b', '')
+        line_list = str(line_list).replace(r'\n', '')
+        line_list = str(line_list).replace(r"'", '')
         window. append(line_list)
 
 
@@ -30,7 +33,7 @@ rows, cols = (9,9)
 #window = [[0 for i in range(cols)] for j in range(rows)]
 window = []
 
-wind_list()
+#wind_list()
 
-for row in window:
-    print(row)
+#for row in window:
+#    print(row)
