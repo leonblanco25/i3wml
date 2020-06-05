2805c09d39fb2f0f635ec1e653f7ba420131e051
leo c <leonardo.cando25@hmail.com>
2020-06-05 00:52:27 -0400

    list bug fixes

--- wdatabase.py
+++ wdatabase.py
@@ -4,35 +4,38 @@ import os
 import subprocess
 
 
+
 def wind_list(list):
     command = "wmctrl -l | awk '{if ($2==\"0\") print $1}' |tr \\n \" \""
     line = command
 
-    # print(command)
     for i in range(11):
-        # print(i)
         line = str(line).replace('0', str(i))
-        # print(line)
         output = subprocess.check_output(line, shell=True)
-        # print("wind " +str(i) + " :" + str(output))
         line = command
+        #print(output)   # checking what comes back from bash
         list_cleanup(output, list)
 
 
 def list_cleanup(input, list):
     stripped_line = input. strip()
-    line_list = stripped_line. split()
-    line_list = str(line_list).replace('b', '')
-    line_list = str(line_list).replace(r'\n', '')
-    line_list = str(line_list).replace(r"'", '')
+    line_list = str(stripped_line). split("\\n")
+    #line_list = str(line_list).strip('b')
+    #line_list = str(line_list).replace(r'\n', '')
+    #line_list = str(line_list).replace('\'', '')
+    #print (line_list) # checking what gets added to list
     list. append(line_list)
 
 
-# rows, cols = (9,9)
-# window = [[0 for i in range(cols)] for j in range(rows)]
-# window = []
 
-# wind_list()
+def debug():
 
-# for row in window:
-#     print(row)
+    ws = []
+
+    wind_list(ws)
+    for i in ws:
+         print(i)
+    print(ws[0][1])
+
+
+#debug()
